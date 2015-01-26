be550780fc4f9745ad2f90154a5a6dda0d14c360
ledancs <andres.ledesma@tut.fi>
2015-01-26 17:39:44 +0200

    Re working the angle visualization into groups. Snap.SVG added.

--- angle-single.js
+++ angle-single.js
@@ -4,6 +4,7 @@ function AngleSingle (svgGroupId, measurements){
     this.width = parseFloat(this.svg.getAttribute("width"));
     this.height = parseFloat(this.svg.getAttribute("height"));
     this.margin = 1;
+    this.s = Snap("#" + svgGroupId);
     this.draw();
 }
 
@@ -17,86 +18,92 @@ AngleSingle.prototype.draw = function (){
     // an additional 50 for the top and bottom margins
     // var svgHeight = 2 * y + height;
     // var svgWidth = x0 * 2 + width;
-    var svgWidth = parseFloat(this.svg.getAttribute("width"));
+    // var svgWidth = parseFloat(this.svg.getAttribute("width"));
     var svgHeight = parseFloat(this.svg.getAttribute("height"));
     // this.svg.setAttribute("height", svgHeight.toString());
     // this.svg.setAttribute("width", svgWidth.toString());
 
     var r0, r1, r2, r3;
-    r0 = 225;
-    r1 = 250;
-    r2 = 450;
-    r3 = 400;
+    // indicators
+    r0 = 140; // the initial point of the low and high indicators
+    r2 = 240; // the end point for the indicators
+    // wellness zone
+    r1 = 140; // the initial point of the wellness zone
+    r3 = 245; // the end point for the wellness zone
+    // the radius of the actual values
+    var valRad0 = 140;
+    var valRad1 = 185;
 
-    var valRad0 = 145;
-    var valRad1 = 335;
+    var centerX = 7;
+    var centerY = svgHeight - 7;
 
-    var centerX = svgWidth/6;
-    var centerY = svgHeight/2;
+    var x1 = centerX + Math.cos(Math.PI/2 * 0.975) * r0;
+    var y1 = centerY - Math.sin(Math.PI/2 * 0.975) * r0;
 
-    var x1 = centerX + Math.cos(Math.PI/2 - 0.2) * r0;
-    var y1 = centerY - Math.sin(Math.PI/2 - 0.2) * r0;
+    var x2 = centerX + Math.cos(Math.PI/2 * 0.975) * r3;
+    var y2 = centerY - Math.sin(Math.PI/2 * 0.975) * r3;
 
-    var x2 = centerX + Math.cos(Math.PI/2 - 0.2) * r3;
-    var y2 = centerY - Math.sin(Math.PI/2 - 0.2) * r3;
+    this.addMinMaxLine(x1 - 2, y1, x2 - 2, y2);
+    this.addMinMaxLabel(x2 - 2, y2, "HIGH", Math.PI/2 * 0.975);
 
-    this.addMinMaxLine(x1, y1, x2, y2, 4);
-    this.addMinMaxLabel(x1, y1 - 75, "HIGH", -77);
+    // PI divided by 12 means that this is the minimum step.
+    // Half of those will not be used so we end up with 6 parts of PI
+    // from 0 to PI/6
+    // the wellness zone will be from PI/6 to PI/3
+    var angle0 = Math.PI/6;
+    var angle1 = Math.PI/3;
 
-    var wellnessAngle = Math.PI/7;
-
-    x1 = centerX + Math.cos(wellnessAngle) * r1;
-    y1 = centerY - Math.sin(wellnessAngle) * r1;
-    x2 = centerX + Math.cos(wellnessAngle) * r2;
-    y2 = centerY - Math.sin(wellnessAngle) * r2;
+    x1 = centerX + Math.cos(angle0) * r1;
+    y1 = centerY - Math.sin(angle0) * r1;
+    x2 = centerX + Math.cos(angle0) * r2;
+    y2 = centerY - Math.sin(angle0) * r2;
 
     var path = "M" + x1.toString() + ", " + y1.toString();
     path += " L " + x2.toString() + " " + y2.toString();
     path += " A " + r2.toString() + " " + r2.toString();
-    path += " 0 0 1";
+    path += " 0 0 0";
 
-    var x3 = centerX + Math.cos(-wellnessAngle) * r1;
-    var y3 = centerY - Math.sin(-wellnessAngle) * r1;
-    var x4 = centerX + Math.cos(-wellnessAngle) * r2;
-    var y4 = centerY - Math.sin(-wellnessAngle) * r2;
+    var x3 = centerX + Math.cos(angle1) * r1;
+    var y3 = centerY - Math.sin(angle1) * r1;
+    var x4 = centerX + Math.cos(angle1) * r2;
+    var y4 = centerY - Math.sin(angle1) * r2;
 
     path += " " + x4.toString() + " " + y4.toString();
     path += " L " + x3.toString() + " " + y3.toString();
 
     path += " A " + r1.toString() + " " + r1.toString();
-    path += " 0 0 0";
+    path += " 0 0 1";
     path += " " + x1.toString() + " " + y1.toString();
 
-    var wellnessZone = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    wellnessZone.setAttribute("d", path);
-    wellnessZone.setAttribute("d", path);
-    wellnessZone.setAttribute("fill", "rgb(150, 150, 150)");
-    wellnessZone.setAttribute("opacity", "0.35");
-
-    this.svg.appendChild(wellnessZone);
+    var wellnessZone = this.s.path(path);
+    wellnessZone.attr({
+        fill: "green",
+        opacity: "0.3"
+    });
 
     // this.addMinMaxLine(x1, y1, x2, y2);
 
     // this.addMinMaxLine(x1, y1, x2, y2);
 
-    x1 = centerX + Math.cos(-Math.PI/2 + 0.2) * r0;
-    y1 = centerY - Math.sin(-Math.PI/2 + 0.2) * r0;
-    x2 = centerX + Math.cos(-Math.PI/2 + 0.2) * r3;
-    y2 = centerY - Math.sin(-Math.PI/2 + 0.2) * r3;
+    x1 = centerX + Math.cos(0) * r0;
+    y1 = centerY - Math.sin(0) * r0;
+    x2 = centerX + Math.cos(0) * r3;
+    y2 = centerY - Math.sin(0) * r3;
 
-    this.addMinMaxLine(x1, y1, x2, y2, 4);
-    this.addMinMaxLabel(x1 - 15, y1 + 70, "LOW", 77);
+    this.addMinMaxLine(x1, y1 + 2, x2, y2 + 2);
+    this.addMinMaxLabel(x2, y2 + 2, "LOW", 0);
 
-    var measurement, scale, angle, line, dot;
+    var measurement, scale, angle, pointer, circle;
 
     for(var i = 0; i < this.measurements.length; i ++){
         measurement = this.measurements[i];
         // the angle goes from - radians to + radians so we multiply by 2 to get the full
-        // spectrum of the angle
-        scale = (wellnessAngle * 2) / (measurement.optimal.max - measurement.optimal.min);
+        // spectrum of the PI/6 to PI/3 so we take this space as the full spectrum of
+        // the distribution of values
+        scale = (angle1 - angle0) / (measurement.optimal.max - measurement.optimal.min);
         angle = measurement.val - measurement.optimal.min;
         angle *= scale;
-        angle -= wellnessAngle;
+        angle += angle0; // this is the minimum value from which we count the optimal range or the wellness zone
 
         x1 = centerX + Math.cos(angle) * valRad0;
         y1 = centerY - Math.sin(angle) * valRad0;
@@ -104,85 +111,50 @@ AngleSingle.prototype.draw = function (){
         x2 = centerX + Math.cos(angle) * valRad1;
         y2 = centerY - Math.sin(angle) * valRad1;
 
-        line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        line.setAttribute("x1", x1.toString());
-        line.setAttribute("y1", y1.toString());
+        pointer = this.s.line(x1, y1, x2, y2);
+        pointer.attr({
+            stroke: "black",
+            strokeWidth: 2
+        });
 
-        line.setAttribute("x2", x2.toString());
-        line.setAttribute("y2", y2.toString());
+        /*circle = this.s.circle(x2, y2, 4).attr({
+            fill: "white",
+            stroke: "black",
+            strokeWidth: 2
+        });*/
 
-        line.setAttribute("stroke", "black");
-        line.setAttribute("stroke-width", "2");
-
-        this.svg.appendChild(line);
-
-        dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        dot.setAttribute("cx", x2.toString());
-        dot.setAttribute("cy", y2.toString());
-        dot.setAttribute("r", "4");
-        dot.setAttribute("fill", "white");
-        dot.setAttribute("stroke", "black");
-        dot.setAttribute("stroke-width", "2");
-
-        this.svg.appendChild(dot);
-
-        this.addLabel(measurement,
-            centerX + Math.cos(angle) * valRad1 * 1.015 ,
-            centerY - Math.sin(angle) * valRad1 * 1.015);
+        this.addLabel(measurement, x2, y2, angle);
     }
-
-
-
 };
 
-AngleSingle.prototype.addMinMaxLine = function (x1, y1, x2, y2, width) {
-    var minLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    minLine.setAttribute("x1", x1.toString());
-    minLine.setAttribute("y1", y1.toString());
-    minLine.setAttribute("x2", x2.toString());
-    minLine.setAttribute("y2", y2.toString());
-    minLine.setAttribute("stroke", "black");
-    minLine.setAttribute("stroke-width", width.toString());
-    this.svg.appendChild(minLine);
+AngleSingle.prototype.addMinMaxLine = function (x1, y1, x2, y2) {
+    var line = this.s.line(x1, y1, x2, y2);
+    line.attr({
+        stroke: "grey",
+        strokeWidth: 5
+    });
 };
 
 AngleSingle.prototype.addMinMaxLabel = function (textX, textY, text, angle) {
-    var minText = document.createElementNS("http://www.w3.org/2000/svg", "text");
-
-    minText.setAttribute("x", textX.toString());
-    minText.setAttribute("y", textY.toString());
-    minText.setAttribute("font-size", "20");
-    var minTextNode = document.createTextNode(text);
-    minText.appendChild(minTextNode);
-    minText.setAttribute("transform", "rotate(" + angle + " " + textX.toString() + " " + textY.toString() + ")");
-
-    var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    var x = textX - 5;
-    var y = textY - 20;
-    rect.setAttribute("fill", "white");
-    rect.setAttribute("x", x.toString());
-    rect.setAttribute("y", y.toString());
-    rect.setAttribute("width", "50");
-    rect.setAttribute("height", "25");
-    // this.svg.appendChild(rect);
-    rect.setAttribute("transform", "rotate(" + angle + " " + textX.toString() + " " + textY.toString() + ")");
-    this.svg.appendChild(minText);
+    var tx = this.s.text(textX + 10, textY + 5, text);
+    tx.attr({
+        fontSize: "20px",
+        fill: "grey"
+    });
+    var degrees = angle * (180/Math.PI);
+    var t = Snap.matrix()
+        .rotate(-degrees, textX, textY);
+    tx.transform(t);
 };
 
-AngleSingle.prototype.addLabel = function (measurement, dotX, dotY) {
-    var newText = document.createElementNS("http://www.w3.org/2000/svg", "text");
-
-    var fontSize = 15;
-
-    var x = dotX + fontSize;
-    var y = dotY + fontSize/5
-
-    newText.setAttribute("x", x.toString());
-    newText.setAttribute("y", y.toString());
-    newText.setAttribute("font-size", fontSize.toString());
-
-    var textNode = document.createTextNode(measurement.label + ": " + measurement.val.toString() + " " + measurement.units);
-    newText.appendChild(textNode);
-
-    this.svg.appendChild(newText);
+AngleSingle.prototype.addLabel = function (measurement, x, y, angle) {
+    var text = measurement.label + ": " + measurement.val.toString() + " " + measurement.units;
+    var tx = this.s.text(x + 5, y + 5, text);
+    tx.attr({
+        fontSize: "15"
+    });
+    var degrees = angle * (180/Math.PI);
+    var t = Snap.matrix()
+        .rotate(-degrees, x, y);
+    tx.transform(t);
 };
